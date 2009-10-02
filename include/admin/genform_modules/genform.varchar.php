ce74eaade2e4203d70ee45d2b6787a4ae36f01df
conort <conort@447f756e-1d47-11de-9433-ad2e3fad96c5>
2009-10-02 12:15:19 +0000

    - Cleaned code

--- include/admin/genform_modules/genform.varchar.php
+++ include/admin/genform_modules/genform.varchar.php
@@ -3,26 +3,33 @@
 if ( !$this->editMode ) {
 
 	/**
-	 * VARCHAR DE TYPE COLOR
+	 * Champ de couleur Hexa
 	 * 
-	 */
-	 
+	 */	 
 	if ( is_array($_Gconfig['colorFields']) && arrayInWord( $_Gconfig['colorFields'], $name ) ) {
+		
 		
 		$this->genHelpImage('help_texte',$name);
     	
-		$fl = $this->tab_field[$name]->max_length;
-	
+		$fl = $this->tab_field[$name]->max_length;  	
     	
-    	
+		/**  
+		 * Couleur par défaut
+		 */
     	if($this->tab_default_field[$name]) {
     		$style = 'style="background-color:#'.$this->tab_default_field[$name].'"';
     	}
 		
+		/**
+		 * Emplacement de couleur
+		 */
     	$this->addBuffer('<span class="colorField" id="colorField_'.$name.'" '.$style.'>
     						&nbsp;   &nbsp;   						
     					 </span> ');
     	
+		/**
+		 * Champ de saisie Couleur
+		 */		 
     	$this->addBuffer(' &nbsp;<input 
 									size="6" 
 									onchange="gid(\'colorField_'.$name.'\').style.backgroundColor=\'#\'+this.value;" 
@@ -31,27 +38,36 @@ if ( !$this->editMode ) {
 									class="genform_varchar" 
 									value=' . alt( $this->tab_default_field[$name] ) . ' />');
     
+		/**
+		 * Lien pour ouverture Popup
+		 */
     	$this->addBuffer(' <a class="btn_spectre" 
 								href="#" 
-								onclick="popup(\'./colorPicker/colorSelector.html?id='.$name.'\',360,240);return false;"
-							>
+								onclick="popup(\'./colorPicker/colorSelector.html?id='.$name.'\',360,240);return false;" >
     					 	<img src="./colorPicker/spectre.jpg" alt="" style="vertical-align:middle"  />
     					 </a>');
     	
 		
 	}
 	else
-	if ( arrayInWord( $_Gconfig['urlFields'], $name ) ) {
+	/**
+	 * Varchar de type URL
+	 */
+	if ( is_array( $_Gconfig['urlFields']) && arrayInWord( $_Gconfig['urlFields'], $name ) ) {
 		
-	
 	    	
 	    	$this->genHelpImage('help_url',$name);
 	    	
-	    	
+			/**
+			 * Valeur par defaut si le champ est vide
+			 */
 	    	if(!strlen(trim($this->tab_default_field[$name]))) {
 	    		$this->tab_default_field[$name] = DEFAULT_URL_VALUE;
 	    	}
 	    	
+			/**
+			 * Champ de saisie
+			 */
 		    $this->addBuffer( '
 		    <input 
 		    		id="genform_'.$name.'" ' . $jsColor . ' 
@@ -61,32 +77,39 @@ if ( !$this->editMode ) {
 		    		maxlength="' . $this->tab_field[$name]->max_length . '" 
 		    		value=' . alt($this->tab_default_field[$name]) . ' /> ' );
 		    
-		
-		        
-		   
-		    
+			/**
+			 * Bouton pour tester le lien
+			 */
 		    $this->addBuffer('
 		    <img src="'.ADMIN_PICTOS_FOLDER.ADMIN_PICTOS_ARBO_SIZE.'/categories/applications-internet.png" 
 		    	onclick="smallPopup(gid(\'genform_'.$name.'\').value)" 
 		    	alt="'.t('tester_le_lien').'" />');
 		    
-				$this->addBuffer('
+			/**
+			 * Bouton pour choisir une rubrique du site
+			 */
+			$this->addBuffer('
 		    <img src="'.ADMIN_PICTOS_FOLDER.ADMIN_PICTOS_ARBO_SIZE.'/status/folder-open.png" 
 		    	onclick="XHR_links(\'genform_'.$name.'\')" 
 		    	alt="'.t('choisir_rubrique').'" />');       
 	
-				  
+			/**
+			 * Conteneur pour l'arbo de sélection
+			 */
 	        $this->addBuffer('<div id="genform_'.$name.'_links" class="xhr_links"></div>');
-	        //$this->addBuffer('<iframe id="genform_'.$name.'_test" src="" ></iframe>');
+
 
 	/**
-	 * VARCHAR DE TYPE MAIL
+	 * VARCHAR DE TYPE EMAIL
 	 * 
 	 */
     } else if ( arrayInWord( $mailFields, $name ) ) {
     	
     	$this->genHelpImage('help_email',$name);
 
+		/** 
+		 * Champ de saisie Email
+		 */
         $this->addBuffer( '<input 
 							id="genform_'.$name.'"  ' . $jsColor . ' '.$attributs.' 
 							type="text" 
@@ -100,9 +123,11 @@ if ( !$this->editMode ) {
      /**
       * Varchar de type Mot de passe
       */
-    } else if(in_array($name,$_Gconfig['passwordFields']) ){
+    } else if( is_array($_Gconfig['passwordFields']) && in_array($name,$_Gconfig['passwordFields']) ){
     	
-		/* Mot de passe avec génération auto */
+		/**
+		* Champ du mot de passe
+		*/
 		$this->addBuffer( '<input 
 							id="genform_'.$name.'" ' . $jsColor . ' 
 							type="text" '.$attributs.' 
@@ -111,31 +136,31 @@ if ( !$this->editMode ) {
 							maxlength="' . $this->tab_field[$name]->max_length . '" 
 							value=' . alt($this->tab_default_field[$name]) . ' />' );
 
+					
 		if(!$this->editMode) {
-
-
-
-//<label  style="float:none;width:200px;" for="generatepassword_'.$name.'" class="abutton"><input src="'.t('src_random_password').'" class="inputimage" type="image" id="generatepassword_'.$name.'"  />'.t('generate_random_password').'</label>');
-		$this->addBuffer( '		
-		
-			<a href="#" 
-				class="titreListe" 
-				style="clear:both;" 
-				id="generatepassword_'.$name.'" >
-				<img src="'.t('src_random_password').'" 
-					class="inputimage" 
-					type="image"  
-					/>'.t('generate_random_password').'</a>');
-		
-			$this->addBuffer( '
-				<script type="text/javascript">
-					$("#generatepassword_'.$name.'").click(
-					function() {
-						$("#genform_'.$name.'").val(generatepass(8));
-						return false;
-					});
-				</script>
-			');
+			/**
+			 * Lien pour génération auto
+			 */
+			$this->addBuffer( '		
+			
+				<a href="#" 
+					class="titreListe" 
+					style="clear:both;" 
+					id="generatepassword_'.$name.'" >
+					<img src="'.t('src_random_password').'" 
+						class="inputimage" 
+						type="image"  
+						/>'.t('generate_random_password').'</a>');
+			
+				$this->addBuffer( '
+					<script type="text/javascript">
+						$("#generatepassword_'.$name.'").click(
+						function() {
+							$("#genform_'.$name.'").val(generatepass(8));
+							return false;
+						});
+					</script>
+				');
 		}
 		
 	
@@ -148,9 +173,16 @@ if ( !$this->editMode ) {
     	
     	$this->genHelpImage('help_texte',$name);
     	
+		/**
+		 * Longueur du Varchar
+		 */
 		$fl = $this->tab_field[$name]->max_length;
 		
+		
     	if( $fl >= 100) {
+			/**
+			 * Si supérieur à 100 => Textarea multiligne
+			 */
 			$this->addBuffer( '<textarea 
 							class="resizable" 
 							id="genform_'.$name.'" ' . $jsColor . ' '.$attributs.'  
@@ -159,6 +191,9 @@ if ( !$this->editMode ) {
 							maxlength="'.$fl.'" 
 							class="genform_varchar" >' . ( $this->tab_default_field[$name] ) . '</textarea>' );
     	} else  {
+			/**
+			 * Si inférieur à 100 => input=type@text
+			 */
         	$this->addBuffer('<input
 								size="'.$fl.'" 
 								id="genform_'.$name.'" 
@@ -172,28 +207,39 @@ if ( !$this->editMode ) {
 								
    			$this->addBuffer($this->genInsertButtons('genform_'.$name.''));
     	}
-    	
-    	//style="width:'.$this->larg.'px" 
+
     }
+	
 } else {
-    /* URL */
+	/**
+	 * Mode visualisation
+	 */
     if (arrayInWord( $_Gconfig['urlFields'], $name ) ) {
+		/**
+		 * Pour les URLs on créé un lien
+		 */
         if ( $this->tab_default_field[$name] ) {
             	$vall = strlen($this->tab_default_field[$name]) > 50 ? 
             			substr($this->tab_default_field[$name],0,50).'...' : 
-            			$this->tab_default_field[$name];
-            	
+            			$this->tab_default_field[$name];            	
             	$this->addBuffer( '<a href="' . $this->tab_default_field[$name] . '" target="_blank">'. ( $vall ) . '</a>' );
-
-            }
-    
-    /* MAIL */        
+		}
+		
     } else if ( arrayInWord( $mailFields, $name ) ) {
-        if ( $this->tab_default_field[$name] )
-            $this->addBuffer( '<a href="mailto:' . $this->tab_default_field[$name] . '" target="_blank">' . 
-            					( $this->tab_default_field[$name] ) . '</a>' );
+		/**
+		 * Pour les mails => Mailto
+		 */
+        if ( $this->tab_default_field[$name] ) {
+            $this->addBuffer( '<a 
+									href="mailto:' . $this->tab_default_field[$name] . '" 
+									target="_blank">' . 
+            					( $this->tab_default_field[$name] ) . '
+								</a>' );
+		}
     } else {
-	
+		/**
+		 * Pour les autres on affiche
+		 */
         $this->addBuffer( ( $this->tab_default_field[$name] ) );
     }
 
