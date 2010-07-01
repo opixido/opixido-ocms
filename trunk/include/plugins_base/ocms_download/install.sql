d8e6f1cf7011f857b21e08077d29cb296123feb5
volgangwindow <volgangwindow@447f756e-1d47-11de-9433-ad2e3fad96c5>
2010-07-01 10:11:08 +0000

    Correction bug plugin ocms_download

--- trunk/include/plugins_base/ocms_download/install.sql
+++ trunk/include/plugins_base/ocms_download/install.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `p_download` (
   `download_id` int(10) unsigned NOT NULL auto_increment,
-  `download_titre__[LG]` varchar(255) NOT NULL,
+  `download_titre_[LG]` varchar(255) NOT NULL,
   `download_fichier_[LG]` varchar(255) NOT NULL,
   `download_ordre` smallint(6) NOT NULL,
   `fk_rubrique_id` int(10) unsigned NOT NULL,
