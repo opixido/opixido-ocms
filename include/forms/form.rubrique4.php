40fda53b72fb4787d4d643c9ea59225a61123f96
conort <conort@447f756e-1d47-11de-9433-ad2e3fad96c5>
2012-05-24 09:43:53 +0000

    - Changed meta information class

--- include/forms/form.rubrique4.php
+++ include/forms/form.rubrique4.php
@@ -1,43 +1,23 @@
 <?php
 
-if($_REQUEST['curId'] != "new")  {
+if ($_REQUEST['curId'] != "new") {
 
-	    if($form->tab_default_field['rubrique_type'] == 'link') {
-        
-	       // $form->genlg("rubrique_link");
-	
-	     //   $form->gen("rubrique_link_en");     
-	        
-        } else if($form->tab_default_field['rubrique_type'] == 'page' ||  $form->tab_default_field['rubrique_type'] == "siteroot") {
-        	
-			p('<h1>'.t('meta_informations').'</h1>');
-		
-	        p('<p>'.t('desc_meta').'</p>');
-	
-	        $form->genlg("rubrique_keywords");
-	
-	        $form->genlg("rubrique_desc");
-	
-	   /*     $form->gen("rubrique_keywords_en");
-	
-	        $form->gen("rubrique_desc_en");*/
-	
-	        
-	
-	    //    $form->gen("rubrique_url_en");
-        
-        } 
-		
-		if($form->tab_default_field['rubrique_type'] != 'link') {
-		
-			$form->genlg("rubrique_url");
-		
-		}
-		
-		
-        
-       
+    if ($form->tab_default_field['rubrique_type'] == 'page' || $form->tab_default_field['rubrique_type'] == "siteroot") {
+
+        if (!$this->editMode) {
+            p('<div class="alert alert-info"><h3>' . t('meta_informations') . '</h3>');
+
+            p('<p>' . t('desc_meta') . '</p></div>');
+        }
+
+        $form->genlg("rubrique_keywords");
+
+        $form->genlg("rubrique_desc");
+    }
+
+    if ($form->tab_default_field['rubrique_type'] != 'link') {
+
+        $form->genlg("rubrique_url");
+    }
 
 }
-
-?>
