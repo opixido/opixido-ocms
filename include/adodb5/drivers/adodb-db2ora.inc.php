52376dc02dc56eca0f44fff90e7d8e8ae20fe046
Celio <celio@opixido.com>
2015-11-30 14:10:20 +0100

    Mise à jour ADODB 5.20.00

--- include/adodb5/drivers/adodb-db2ora.inc.php
+++ include/adodb5/drivers/adodb-db2ora.inc.php
@@ -1,13 +1,15 @@
 <?php
-/* 
-V5.16 26 Mar 2012  (c) 2000-2012 John Lim (jlim#natsoft.com). All rights reserved.
-  Released under both BSD license and Lesser GPL library license. 
-  Whenever there is any discrepancy between the two licenses, 
-  the BSD license will take precedence. 
+/*
+@version   v5.20.0  28-Nov-2015
+@copyright (c) 2000-2013 John Lim (jlim#natsoft.com). All rights reserved.
+@copyright (c) 2014      Damien Regad, Mark Newnham and the ADOdb community
+  Released under both BSD license and Lesser GPL library license.
+  Whenever there is any discrepancy between the two licenses,
+  the BSD license will take precedence.
 Set tabs to 4 for best viewing.
-  
+
   Latest version is available at http://adodb.sourceforge.net
-  
+
   Microsoft Visual FoxPro data driver. Requires ODBC. Works only on MS Windows.
 */
 
@@ -35,46 +37,39 @@ global $_COLONARR,$_COLONSZ;
 
 	$_COLONARR = array();
 	$_COLONSZ = sizeof($arr);
-	
+
 	$sql2 = preg_replace("/(:[0-9]+)/e","_colontrack('\\1')",$sql);
-	
+
 	if (empty($_COLONARR)) return array($sql,$arr);
-	
+
 	foreach($_COLONARR as $k => $v) {
-		$arr2[] = $arr[$v]; 
+		$arr2[] = $arr[$v];
 	}
-	
+
 	return array($sql2,$arr2);
 }
 
 class ADODB_db2oci extends ADODB_db2 {
-	var $databaseType = "db2oci";	
+	var $databaseType = "db2oci";
 	var $sysTimeStamp = 'sysdate';
 	var $sysDate = 'trunc(sysdate)';
-	
-	function ADODB_db2oci()
-	{
-		$this->ADODB_db2();
-	}
-	
-	
-	function _Execute($sql, $inputarr)
+
+	function _Execute($sql, $inputarr = false)
 	{
 		if ($inputarr) list($sql,$inputarr) = _colonscope($sql, $inputarr);
 		return parent::_Execute($sql, $inputarr);
 	}
 };
- 
 
-class  ADORecordSet_db2oci extends ADORecordSet_odbc {	
-	
-	var $databaseType = "db2oci";		
-	
-	function ADORecordSet_db2oci($id,$mode=false)
+
+class  ADORecordSet_db2oci extends ADORecordSet_odbc {
+
+	var $databaseType = "db2oci";
+
+	function __construct($id,$mode=false)
 	{
-		return $this->ADORecordSet_db2($id,$mode);
+		return parent::__construct($id,$mode);
 	}
 }
 
 } //define
-?>
