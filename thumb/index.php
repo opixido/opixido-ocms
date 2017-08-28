254f6e5e0954362d394fc34feb090c5c108c6fa3
opixido-celio <celio@opixido.com>
2017-08-28 14:00:01 +0000

    Update phpthumb

--- thumb/index.php
+++ thumb/index.php
@@ -18,6 +18,30 @@ if (ini_get('magic_quotes_runtime')) {
 }
 $starttime = array_sum(explode(' ', microtime()));
 
+if(!empty($_REQUEST['c'])) {
+    unset($_REQUEST['c']);
+}
+if(empty($_REQUEST)) {
+   $x = explode('?',($_SERVER['REQUEST_URI']));
+    $_SERVER['REQUEST_URI'] = $x[0];
+   $x = explode('/src/',$x[0]);
+
+   $params = explode('/',$x[0]);
+
+   $src = '/'.urldecode($x[1]);
+   $_GET = $_REQUEST = array();
+   $_GET['src'] = $_REQUEST['src'] = $src;
+   $_SERVER['QUERY_STRING'] = 'src='.$src;
+   foreach($params as $v) {
+        $x = explode('__',$v);
+        if(!empty($x[1])) {
+           $_GET[$x[0]] = $_REQUEST[$x[0]] = $x[1];
+           $_SERVER['QUERY_STRING'] .= '&'.$x[0].'='.$x[1];
+       }
+   }
+}
+
+
 // this script relies on the superglobal arrays, fake it here for old PHP versions
 if (phpversion() < '4.1.0') {
     $_SERVER = $HTTP_SERVER_VARS;
