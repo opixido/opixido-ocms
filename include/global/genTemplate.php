f2b133eda901ac9d33093f8c85098897ed369b3c
Ce Lio <celio@opixido.com>
2020-12-02 12:11:23 +0000

    Fix genTemplate when using arrays or objects

--- include/global/genTemplate.php
+++ include/global/genTemplate.php
@@ -135,7 +135,11 @@ class genTemplate
         $this->vars[ $nom ] = $valeur;
 
         if ($this->doreplace && !in_array('@@' . $nom . '@@', $this->replaces) && !is_array($valeur) && !is_object($valeur)) {
-            $this->replaces[] = '@@' . $nom . '@@';
+            if (!is_array($valeur) && !is_object($valeur)) {
+                $this->replaces[] = '@@' . $nom . '@@';
+            } else {
+                $this->replaces[] = '';
+            }
         }
 
         return $this;
