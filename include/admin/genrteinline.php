f5d20b5447afba2d07c14416c470e1721ff1f952
Ce Lio <celio@opixido.com>
2021-08-25 09:47:42 +0000

    Remove tinymce include from genrteinline as it is now forced in footer if rteElements are defined

--- include/admin/genrteinline.php
+++ include/admin/genrteinline.php
@@ -16,27 +16,31 @@
  * You should have received a copy of the GNU General Public License
  * along with oCMS. If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Celio Conort / Opixido 
+ * @author Celio Conort / Opixido
  * @copyright opixido 2012
  * @link http://code.google.com/p/opixido-ocms/
  * @package ocms
  * */
-class genRteInline {
+class genRteInline
+{
 
     var $toolbar;
 
-    function __construct($champ, $valeur = "", $toolbar = 'Default', $tabContent = '') {
+    function __construct($champ, $valeur = "", $toolbar = 'Default', $tabContent = '')
+    {
         $this->toolbar = $toolbar;
         $this->tabContent = $tabContent;
         $this->champ = $champ;
         $this->valeur = $valeur;
     }
 
-    function gen() {
-        return $this->createRte($content);
+    function gen()
+    {
+        return $this->createRte();
     }
 
-    function createRte() {
+    function createRte()
+    {
         global $formFooters, $champsRTE;
 
         $formFooters = '';
@@ -44,23 +48,11 @@ class genRteInline {
 
         $html = $this->instanceRte();
 
-
-
-//separator,insertdate,inserttime,print,help <script language="javascript" type="text/javascript"> //xhtmlxtras,accessilink,iespell,insertdatetime,searchreplace,print,contextmenu,paste,styleselect
-
-        if (!$GLOBALS['rteIncluded']) {
-            $formFooters .= ' 
-		    <script src="../vendor/tinymce/tinymce/tinymce.min.js"></script>
-			';
-        }
-
-        $GLOBALS['rteIncluded'] = true;
-
-
         return $html . $formFooters;
     }
 
-    function instanceRte() {
+    function instanceRte()
+    {
 
         $html = ('<textarea  name="' . $this->champ . '" id="' . $this->champ . '" 
 						style="height:300px;width:500px" > ' . $this->valeur . ' </textarea >
