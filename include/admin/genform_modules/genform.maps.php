a79e61f61be7312e05b79ec4abc3959c7e9ff397
Ce Lio <celio@opixido.com>
2024-02-16 17:12:36 +0000

    Mise à jour du genform.maps en OSM

--- include/admin/genform_modules/genform.maps.php
+++ include/admin/genform_modules/genform.maps.php
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with oCMS. If not, see <http://www.gnu.org/licenses/>.
 #
-# @author Celio Conort / Opixido 
+# @author Celio Conort / Opixido
 # @copyright opixido 2012
 # @link http://code.google.com/p/opixido-ocms/
 # @package ocms
@@ -59,16 +59,18 @@ if (!$this->editMode) {
 
     $this->addBuffer('<div class="map_field" id="map_field_' . $name . '">');
 
-    $this->addBuffer('<button class="button geocode">' . t('map_geocode') . '</button>');
-    $this->addBuffer('<button class="button usecenter">' . t('map_usecenter') . '</button>');
+//    $this->addBuffer('<button class="button geocode">' . t('map_geocode') . '</button>');
+    //  $this->addBuffer('<button class="button usecenter">' . t('map_usecenter') . '</button>');
 
     $this->addBuffer('<div class="clearer" ></div>');
 
     $this->addBuffer('<div class="map_map">');
-    $this->addBuffer('<div id="map_field_' . $name . '_map" style="width:570px;height:400px;border:1px solid">MAP</div>');
+    $this->addBuffer('<div id="map_field_' . $name . '_map" style="width:100%;height:400px;border:1px solid"></div>');
     $this->addBuffer('</div>');
 
     $this->addBuffer('<div class="map_latlonfields" >');
+
+    $this->addBuffer('<label for="genform_latLong" >' . t('genform_latLong'). '</label><input type="text" id="genform_latLong" name="genform_latLong" value="' . $this->tab_default_field[$chp_lat] . ',' . $this->tab_default_field[$chp_lon] . '" />');
 
     $this->addBuffer('<label for="genform_' . $chp_lat . '" >' . t('genform_' . $chp_lat) . '</label><input type="text" id="genform_' . $chp_lat . '" name="genform_' . $chp_lat . '" value="' . $this->tab_default_field[$chp_lat] . '" />');
     $this->addBuffer('<label for="genform_' . $chp_lon . '" >' . t('genform_' . $chp_lon) . '</label><input type="text" id="genform_' . $chp_lon . '" name="genform_' . $chp_lon . '" value="' . $this->tab_default_field[$chp_lon] . '" />');
@@ -80,86 +82,110 @@ if (!$this->editMode) {
     $this->bufferPrint = '';
     ?>
 
-    <script type="text/javascript"
-            src="https://maps.googleapis.com/maps/api/js?sensor=false">
-    </script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"/>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-geosearch@3.2.1/dist/geosearch.css"/>
+    <script src="https://unpkg.com/leaflet-geosearch@3.2.1/dist/geosearch.umd.js"></script>
 
     <script type="text/javascript">
 
+        $(function () {
 
-        $('#map_field_<?php echo $name ?> button.usecenter').click(function () {
-            marker.setPosition(map.getCenter());
-            $('#genform_<?php echo $chp_lat ?>').val(map.getCenter().lat());
-            $('#genform_<?php echo $chp_lon ?>').val(map.getCenter().lng());
-            return false;
-        });
 
-        $('#map_field_<?php echo $name ?> button.geocode').click(function () {
-            var fields = <?php echo json_encode($chpsAuto2) ?>;
-            var address = '';
-            for (p in fields) {
-                t = fields[p];
-                if ($("#genform_" + t).length) {
-                    address += $("#genform_" + t).val() + ' ';
-                } else {
-                    address += t + ' ';
-                }
+            var curLocation = [$("#<?='genform_' . $chp_lat ?>").val(), $("#<?='genform_' . $chp_lon ?>").val()];
+
+            if (curLocation[0] == 0 && curLocation[1] == 0) {
+                curLocation = [48.8534, 2.34];
             }
-            codeAddress(address);
-            return false;
-        });
-
-        var latlng = new google.maps.LatLng(<?= choose($this->tab_default_field[$chp_lat],"48.8536") ?>, <?= choose($this->tab_default_field[$chp_lon],"2.3934") ?>);
-        var myOptions = {
-            zoom: 8,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("map_field_<?php echo $name ?>_map"), myOptions);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: latlng,
-            draggable: true
-        });
-
-        google.maps.event.addListener(marker, 'position_changed', function () {
-            $('#genform_<?php echo $chp_lat ?>').val(marker.getPosition().lat());
-            $('#genform_<?php echo $chp_lon ?>').val(marker.getPosition().lng());
-        });
 
 
-        var geocoder = new google.maps.Geocoder();
+            var map = L.map("<?='map_field_' . $name . '_map'?>").setView(curLocation, 10);
 
-
-        function codeAddress(address) {
-            geocoder.geocode({'address': address}, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-
-                    map.setCenter(results[0].geometry.location);
-                    marker.setPosition(results[0].geometry.location);
-                    /* marker = new google.maps.Marker({
-                     map: map,
-                     position: results[0].geometry.location
-                     });
-
-
-                     $('#genform_
-                    <?php echo $chp_lat ?>').val(results[0].geometry.location.lat());
-                     $('#genform_
-                    <?php echo $chp_lon ?>').val(results[0].geometry.location.lng());
-                     */
-                } else {
-                    alert("Geocode was not successful for the following reason: " + status);
-                }
+            const search = new GeoSearch.GeoSearchControl({
+                style: 'bar',
+                provider: new GeoSearch.OpenStreetMapProvider(),
+                updateMap: true,
+                showMarker: false,
+                showPopup: false
             });
-        }
 
+            map.addControl(search);
+
+
+            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/streets-v11',
+            }).addTo(map);
+            map.attributionControl.setPrefix(false);
+
+            var marker = new L.marker(curLocation, {
+                draggable: 'true'
+            });
+
+            marker.on('dragend', function (event) {
+                var position = marker.getLatLng();
+                marker.setLatLng(position, {
+                    draggable: 'true'
+                }).bindPopup(position).update();
+                $("#<?='genform_' . $chp_lat ?>").val(position.lat);
+                $("#<?='genform_' . $chp_lon ?>").val(position.lng).keyup();
+            });
+
+            $("#<?='genform_' . $chp_lat ?>, #<?='genform_' . $chp_lon ?>").change(function () {
+                var position = [parseFloat($("#<?='genform_' . $chp_lat ?>").val()), parseFloat($("#<?='genform_' . $chp_lon ?>").val())];
+                marker.setLatLng(position, {
+                    draggable: 'true'
+                }).bindPopup(position).update();
+                map.panTo(position);
+                $("#genform_latLong").val([$("#<?='genform_' . $chp_lat ?>").val(),$("#<?='genform_' . $chp_lon ?>").val()]);
+            });
+            $("#genform_latLong").change(function () {
+                var tabLatLong = $(this).val().split(',');
+                var position = [parseFloat(tabLatLong[0]), parseFloat(tabLatLong[1])];
+                marker.setLatLng(position, {
+                    draggable: 'true'
+                }).bindPopup(position).update();
+                map.panTo(position);
+                $("#<?='genform_' . $chp_lat ?>").val(tabLatLong[0]);
+                $("#<?='genform_' . $chp_lon ?>").val(tabLatLong[1]).keyup();
+            });
+
+            map.addLayer(marker);
+
+
+            function onMapClick(e) {
+                var center = e.latlng;
+                marker.setLatLng(center);
+                $("#genform_latLong").val(center.lat + ',' + center.lng);
+
+                $("#<?='genform_' . $chp_lat ?>").val(center.lat);
+                $("#<?='genform_' . $chp_lon ?>").val(center.lng).keyup();
+            };
+
+            map.on('click', onMapClick);
+
+            $('#map_field_<?php echo $name ?> button.usecenter').click(function (e) {
+                e.preventDefault();
+                marker.setLatLng(map.getCenter());
+
+                $('#genform_<?php echo $chp_lat ?>').val(map.getCenter().lat);
+                $('#genform_<?php echo $chp_lon ?>').val(map.getCenter().lng);
+                return false;
+            });
+
+
+        });
     </script>
 
+    <style type="text/css">
+        #genform_formulaire .leaflet-control-geosearch input[type=text] {
+            width: 100%;
+        }
+    </style>
 
 
-<?php
+    <?php
 } else {
 
 
